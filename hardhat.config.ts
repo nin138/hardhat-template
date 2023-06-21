484a1743bfe8a2db643ae77193e6d61b8115828f
nin138 <ninian138@gmail.com>
2023-06-21 15:32:40 +0900

    prepare to development

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,20 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "dotenv/config";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.18",
+  solidity: {
+    version: "0.8.18",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+    etherscan: {
+      apiKey: {},
+    },
+  },
 };
 
 export default config;
